59d18d49ba743b15973ea753bcaf36e4a84b8df7
Mayohills <manou.lskr@gmail.com>
2024-11-14 10:37:08 +0100

    script werkt

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -2,13 +2,21 @@ import click
 import numpy as np
 from numpy import pi
 import pandas as pd
+import sys
 
+@click.group()
+def cmd_group():
+    pass
+
+@cmd_group.command()
+@click.option("-n", "--number", default = 10)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@cmd_group.command()
+@click.option("-n", "number", default = 10)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -16,4 +24,5 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
+ 
